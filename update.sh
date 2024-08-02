8feab87a7e88fd4298489445b8227aaa36ed4bfe
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 07:57:20 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -3,17 +3,12 @@
 
 light_green='\033[1;32m'
 nc='\033[0m'
-printf "${light_green}pythonPractices updating...${nc}\]"
-data=('phonenumbers', 'sys')
-for i in "${data[@]}"
-do
-pkg install "${i}"
-done
+printf "${light_green}pythonPractices updating...${nc}"
 
 rm -rf
 git clone https://github.com/reyyymartt/pythonPractices.git
 
-printf "${light_green}file updated${nc}\]"
+printf "${light_green}file updated${nc}"
 ls
 
 
