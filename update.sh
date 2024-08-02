af0da49d43b8823bf89c2d4e71e67d9f52d9cdee
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-02 12:14:47 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ light_green='\033[1;32m'
 nc='\033[0m'
 
 printf "${light_green}pythonPractices updating...${nc}"
+
+cd
 rm -rf pythonPractices
 git clone https://github.com/reyyymartt/pythonPractices.git
 
