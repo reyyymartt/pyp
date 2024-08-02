f13fec9c61dcf40f4aebcb3cee90ede642151887
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-02 12:44:07 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -6,9 +6,10 @@ nc='\033[0m'
 
 printf "${light_green}pythonPractices updating...${nc}"
 
+cd
 rm -rf pythonPractices
 git clone https://github.com/reyyymartt/pythonPractices.git
 printf "${light_green}file updated${nc}"
 ls
 printf "${light_green}exit${nc}"
-exit 0
+exit 1
