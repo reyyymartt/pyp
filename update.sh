3769554175728d86b1ef22235439a8587190f30a
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 06:52:21 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 
 light_green='\033[1;32m'
 nc='\033[0m'
-printf "${light_green}pythonPractices updating...${nc}"
+printf "${light_green}pythonPractices updating...${nc}\]"
 data=('phonenumbers', 'sys')
 for i in "${data[@]}"
 do
@@ -13,7 +13,7 @@ done
 rm -rf
 git clone https://github.com/reyyymartt/pythonPractices.git
 
-printf "${light_green}file updated${nc}"
+printf "${light_green}file updated${nc}\]"
 ls
 
 
