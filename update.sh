df08a6c8e52266506db566de153c0d15688bd7de
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-02 12:36:58 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -9,5 +9,6 @@ printf "${light_green}pythonPractices updating...${nc}"
 rm -rf pythonPractices
 git clone https://github.com/reyyymartt/pythonPractices.git
 printf "${light_green}file updated${nc}"
+pythonPractices
 ls
 exit 
