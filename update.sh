3045e648cc73fcf1699266856bd54f01e1038428
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 06:43:48 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ light_green='\033[1;32m'
 nc='\033[0m'
 printf "${light_green}pythonPractices updating...${nc}"
 data=('phonenumbers')
-for i in '${data[@]}' do
+for i in "${data[@]}"
+do
 printf ("${i}")
 done
 
