f9b1b83f07beb1fa8147cfa94c3f9c53897288e6
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 08:36:44 +0800

    Add files via upload

--- start.bash
+++ start.bash
@@ -2,17 +2,10 @@
 #! /bin/bash
 
 echo "---Installing Packages---"
-Packages=(
-  'pkg install phonenumbers',
-  'pkg install sys',
-  'pip install PIL'
-  )
 
-for package in "${Packages[@]}"
-do
-echo package
-printf "${package} added"
-done
+pkg install phonenumbers
+pkg install sys
+pip install PIL
 
 echo "App starting..."
 python3 numberinfo.py
