ba250fc119033ebc42922890562b020e0ecbb229
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 17:29:55 +0800

    Update functionsModule.py

--- functionsModule.py
+++ functionsModule.py
@@ -6,15 +6,28 @@ import phonenumbers
 import colorama
 
 from colorama import Fore, Back, Style
-
+from phonenumbers import timezone,geocoder,carrier
 
 def Func1(args):
   print(Fore.RED + Back.GREEN + 'Function called' + Style.RESET_ALL)
   
-
+def numberInfo (args):
+  print("input phonenumber, with country code")
+  num=input(Fore.RED+">> "+Style.RESET_ALL)
+  phone = phonenumbers.parse(num)
+  time = timezone.time_zones_for_number(phone)
+  carry = carrier.name_for_number(phone,"en")
+  country = geocoder.description_for_number(phone,"en")
+  print(phone)
+  print(time)
+  print(carry)
+  print(country)
 
 functions = {
-"func1":{
-"func": Func1
-}
+  "testfunc":{
+    "function": Func1
+  },
+  "numberinfo":{
+    "function": numberInfo
+  }
 }
