65f87a559c3cdb333732a597fe23c0b98c0230e2
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 09:29:55 +0800

    Add files via upload

--- update.bash
+++ update.bash
@@ -3,14 +3,14 @@
 
 light_green='\033[1;32m'
 nc='\033[0m'
-printf "${light_green}pythonPractices updating...${nc}"
+printf "${light_green}system updating...${nc}"
 
 cd
-rm -rf pythonPractices
+rm -rf pyp
 git clone https://github.com/reyyymartt/pyp.git
 
 printf "${light_green}file updated${nc}"
-cd pythonPractices
+cd pyp
 ls
 
 
