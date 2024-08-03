0efa09c07e45cf88c09598353ebc0d275fdd325a
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 12:56:29 +0800

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,13 +1,16 @@
 
 #! /bin/bash
+light_green='\e[1;32m'
+yellow='\e[33m'
+nc='\e[0m'
 
-echo "---Installing Packages---"
+echo -e "${light_green}---Installing Packages---${nc}"
 
 pkg install phonenumbers
 pkg install sys
 pip install colorama
 
-echo "App loaded"
+echo -e "${yellow}All set, program started!${nc}"
 python3 system.py
 
 
