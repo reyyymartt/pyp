cb323e425f31642278f48a448ac5b65c24ac1192
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 10:01:53 +0800

    Update update.bash

--- update.bash
+++ update.bash
@@ -5,13 +5,11 @@ light_green='\033[1;32m'
 nc='\033[0m'
 printf "${light_green}system updating...${nc}"
 
-cd
 rm -rf pyp
 git clone https://github.com/reyyymartt/pyp.git
 
 printf "${light_green}file updated${nc}"
 cd pyp
-find pyp
 ls
 
 clear
