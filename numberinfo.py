1a6c0ec3b6b610a0227e6553d8adf8b0d6190a3c
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 08:51:04 +0800

    Add files via upload

--- numberinfo.py
+++ numberinfo.py
@@ -11,13 +11,17 @@ from phonenumbers import timezone,geocoder,carrier
 print("Input your phone number")
 num = input("Phone number: ")
 
-phone = phonenumbers.parse(num)
-time = timezone.time_zones_for_number(phone)
-carry = carrier.name_for_number(phone,"en")
-country = geocoder.description_for_number(phone,"en")
+if (num=="exit"):
+  print("cancelled")
+else:
+  phone = phonenumbers.parse(num)
+  time = timezone.time_zones_for_number(phone)
+  carry = carrier.name_for_number(phone,"en")
+  country = geocoder.description_for_number(phone,"en")
+  print(phone)
+  print(time)
+  print(carry)
+  print(country)
 
 
-print(phone)
-print(time)
-print(carry)
-print(country)
+
