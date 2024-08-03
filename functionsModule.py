91d46dc3b7f238552a5374d7bf1236fb9d8ce51a
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-03 12:07:46 +0800

    Add files via upload

---
+++ functionsModule.py
@@ -0,0 +1,21 @@
+
+
+import time
+import sys
+import phonenumbers
+import colorama
+
+from colorama import Fore, Back, Style
+
+
+def Func1(args):
+  print(Fore.RED + Back.GREEN + 'Function called')
+  print(Style.RESET_ALL)
+  print(args)
+
+
+functions = {
+"func1":{
+"func": Func1
+}
+}
--- system.py
+++ system.py
@@ -1,1 +1,29 @@
 
+
+import json
+import time, sys
+import phonenumbers
+import functionsModule
+
+from functionsModule import functions
+
+def input_():
+  inp = input("> ")
+  split = inp.split(" ")
+  arguments=[]
+  for x in range(len(split)):
+  	arguments.append(split[x].lower())
+
+  if (split[0]=="exit"):
+    return False
+  elif(split[0]=="exec" and len(split)==2):
+    if (split[1].lower() in functions):
+    	functions[split[1].lower()]["func"](arguments)
+    else:
+    	print(split[1],"not found")
+    
+    
+  input_()
+    
+    
+input_()
