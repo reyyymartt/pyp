92da9f0e2acec7a3afdb91ef93d76886bc158be5
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-04 11:38:24 +0800

    Update dataModule.py

--- dataModule.py
+++ dataModule.py
@@ -1,9 +1,24 @@
 
 import functionsModule
+import colorama
 
-command_functions {}
+from functionsModule import functions
+from colorama import Fore, Back, style
 
-execute_functions = {
-  
+
+def ignore(str, args):
+  return False
+
+def executeFunction (str, args):
+  if (len(args)>=1):
+    check=(args[1] in functions) or False
+    if (check==True):
+      functions[args[1]]["function"](args)
+    else:
+      print("couldn't find["+Fore.RED+args[1]+Style.RESET_ALL+'] as a function')
+
+
+command_functions {
+  "exit": ignore,
+  "exe": executeFunction
 }
-
