1db0429dd20924b2eb961677fcb139b06e3a16b0
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-05 22:14:22 +0800

    Update dataModule.py

--- dataModule.py
+++ dataModule.py
@@ -2,8 +2,9 @@
 import functionsModule
 import colorama
 
-from functionsModule import functions
+from functionsModule import functions, searCH
 from colorama import Fore, Back, Style
+
 
 def logoutApp():
   print("end")
@@ -18,7 +19,13 @@ def executeFunction (str, args):
       functions[args[1]]["function"](args)
     else:
       print("no function called {"+Fore.RED+args[1]+Style.RESET_ALL+'}')
-
+      table={}
+      for x in functions:
+        table.add(x)
+      search=searCH(table,args[1])
+      if (len(search)>0):
+        for a in search:
+          print("Did you mean "+Fore.YELLOW+args[1]+Style.RESET_ALL)
 
 command_functions={
   "exit": ignore,
