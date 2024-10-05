61d896c09fd5825c0ab4d783ec65628dd12784db
rem <86702826+reyyymartt@users.noreply.github.com>
2024-10-06 00:44:33 +0800

    Update update.bash

--- update.bash
+++ update.bash
@@ -5,13 +5,10 @@ light_green='\e[1;32m'
 yellow='\e[33m'
 nc='\e[0m'
 echo -e "${yellow}system updating...${nc}"
-
 cd 
 rm -rf pyp
 git clone https://github.com/reyyymartt/pyp.git
-
 echo -e "${light_green}file updated${nc}"
-
 clear
 echo -e "${yellow}System updated!${nc}"
 cd pyp
