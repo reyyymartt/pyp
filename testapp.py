109cce1cece536d13919514394567e16f92fea18
rem <reymartcenteno03@gmail.com>
2024-10-14 14:49:26 +0800

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -7,7 +7,10 @@ def getAuthor ():
     with open("data.json", "r") as file:
         data=json.load(file)
         return data["Author"]
+def index():
+    with open("test.html", "r") as page:
+        return page.read()
 
 @app.route("/")
 def hello_world():
-    return f"<p>Hello, {getAuthor()}!</p>"
+    return f"{index()}"
