672305d7abae7d50a86247a1277073d44f3b0e94
rem <reymartcenteno03@gmail.com>
2024-10-16 05:36:44 +0800

    Update dataModule.py

--- dataModule.py
+++ dataModule.py
@@ -26,9 +26,9 @@ def executeFunction (str, args):
   if (len(args)>1):
     check=(args[1] in functions) or False
     if (check==True):
-      functions[args[1]]["function"](args)
-      text = f"{user} has called the function {args[1]}"
+      text = f"{user} has called the function {args[1]} \n with arguments:{args}"
       log(text)
+      functions[args[1]]["function"](args)
     else:
       print("no function called {"+Fore.RED+args[1]+Style.RESET_ALL+'}')
       table=[]
