3f6f2c7eb24d2998272726fef954378a64658cc4
rem <reymartcenteno03@gmail.com>
2024-10-23 15:30:07 +0800

    Update index.py

--- index.py
+++ index.py
@@ -20,7 +20,6 @@ def updateUser():
     file.seek(0)
     data=json.load(file)
     config["user"] = data["Author"]
-    file.close()
 
 def function(str, args):
   command_functions[str](str, args)
