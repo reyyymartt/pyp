68b479916da445a4a01a7d2b062fa9d8e4c4981d
rem <reymartcenteno03@gmail.com>
2024-10-23 14:47:19 +0800

    Update functionsModule.py

--- functionsModule.py
+++ functionsModule.py
@@ -51,8 +51,10 @@ def viewData (args):
   answer=input("New author: ")
   if (answer!=""):
     newuser=None
+    prevUser=None
     with open(dbName, 'r+') as file:
       data = json.load(file)
+      prevUser=data["Author"]
       data["Author"] = answer
       newuser=answer
       file.seek(0)
@@ -60,7 +62,7 @@ def viewData (args):
       file.truncate
       print(data["Author"])
       file.close()
-      log(f"username changed to {newuser}")
+      log(f"username changed from {prevUser} to {newuser}")
   else:
       print(colortext("Missing argument 1","red"))
     
