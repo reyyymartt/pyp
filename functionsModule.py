d239235ed190133f2375d1d8562c77d71694ab19
rem <reymartcenteno03@gmail.com>
2024-10-23 15:38:00 +0800

    Update functionsModule.py

--- functionsModule.py
+++ functionsModule.py
@@ -62,7 +62,7 @@ def viewData (args):
       file.truncate
       print(data["Author"])
       file.close()
-      log(f"username changed from {colortext(prevUser,"yellow")} to {colortext(newuser,"green")}")
+      log(f"username changed from {colortext(prevUser,"yellow")} to {colortext(newuser,"yellow")}")
   else:
       print(colortext("Missing argument 1","red"))
     
